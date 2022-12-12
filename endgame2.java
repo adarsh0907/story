2dce19a884eeee5ab3ac871f82f0737ab7925d9f
Patel <adarsh.a.a.patel@accenture.com>
2022-12-13 04:31:38 +0900

    endgame2 for factorial

---
+++ endgame2.java
@@ -0,0 +1,9 @@
+public class endgame2 {
+    public static void main(String[] args) {
+        int count=1;
+        for(int i=1;i<10;i++){
+            count*=i;
+        }
+        System.out.println(count);
+    }
+}
